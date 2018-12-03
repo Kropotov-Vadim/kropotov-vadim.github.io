c239582fa9a74575785986491c049f24545b0e2d
Kropotov-Vadim <41684180+Kropotov-Vadim@users.noreply.github.com>
2018-12-03 11:21:36 +0300

    bino short

--- bino-short/js/main.js
+++ bino-short/js/main.js
@@ -46,71 +46,69 @@ var namei = document.getElementsByClassName("form_input")[0],
   subj = document.getElementsByClassName("form_input")[2],
   tarea = document.getElementsByClassName("form_textarea")[0],
   subm = document.getElementsByClassName("btn-form")[0],
-  formThx= document.getElementsByClassName("form-thx")[0];
+  formThx= document.getElementsByClassName("form-thx")[0],
+  error = "error",
+  alright = "good";
 
 mail.onblur = function () {
   var result = this.value.match(/^[0-9a-z-\.]+\@[0-9a-z-]{1,}\.[a-z]{2,}$/i);
   if (!result) {
-    mail.classList.remove("good");
-    mail.classList.add("error");
+    mail.classList.remove(alright);
+    mail.classList.add(error);
   } else {
-    mail.classList.remove("error");
-    mail.classList.add("good");
+    mail.classList.remove(error);
+    mail.classList.add(alright);
   }
 }
 
 mail.onfocus = function () {
-  mail.classList.remove("error");
-  mail.classList.remove("good");
-  mail.style.borderBottomColor = "#999";
+  mail.classList.remove(error);
+  mail.classList.remove(alright);
 }
 
 namei.onblur = function () {
   if (namei.value == "") {
-    namei.classList.remove("good");
-    namei.classList.add("error");
+    namei.classList.remove(alright);
+    namei.classList.add(error);
   } else {
-    namei.classList.remove("error");
-    namei.classList.add("good");
+    namei.classList.remove(error);
+    namei.classList.add(alright);
   }
 }
 
 namei.onfocus = function () {
-  namei.classList.remove("error");
-  namei.classList.remove("good");
-  namei.style.borderBottomColor = "#999";
+  namei.classList.remove(error);
+  namei.classList.remove(alright);
 }
 
 subj.onblur = function () {
   if (subj.value == "") {
-    subj.classList.remove("good");
-    subj.classList.add("error");
+    subj.classList.remove(alright);
+    subj.classList.add(error);
   } else {
-    subj.classList.remove("error");
-    subj.classList.add("good");
+    subj.classList.remove(error);
+    subj.classList.add(alright);
   }
 }
 
 subj.onfocus = function () {
   subj.classList.remove("error");
-  subj.classList.remove("good");
-  subj.style.borderBottomColor = "#999";
+  subj.classList.remove(alright);
 }
 
 tarea.onblur = function () {
   if (tarea.value == "") {
-    tarea.classList.remove("good");
-    tarea.classList.add("error");
+    tarea.classList.remove(alright);
+    tarea.classList.add(error);
   } else {
-    tarea.classList.remove("error");
-    tarea.classList.add("good");
+    tarea.classList.remove(error);
+    tarea.classList.add(alright);
   }
 }
 
 tarea.onfocus = function () {
-  tarea.classList.remove("error");
-  tarea.classList.remove("good");
-  tarea.style.borderBottomColor = "#999";
+  tarea.classList.remove(error);
+  tarea.classList.remove(alright);
 }
 
 subm.addEventListener("click", function() {
@@ -118,14 +116,14 @@ subm.addEventListener("click", function() {
 
   if ( !result || namei.value == "" || tarea.value == "" || subj.value == "") {
     event.preventDefault();
-    mail.classList.remove("good");
-    tarea.classList.remove("good");
-    subj.classList.remove("good");
-    namei.classList.remove("good");
-    mail.classList.add("error");
-    tarea.classList.add("error");
-    subj.classList.add("error");
-    namei.classList.add("error");
+    mail.classList.remove(alright);
+    tarea.classList.remove(alright);
+    subj.classList.remove(alright);
+    namei.classList.remove(alright);
+    mail.classList.add(error);
+    tarea.classList.add(error);
+    subj.classList.add(error);
+    namei.classList.add(error);
   } else {
     event.preventDefault();
     subm.style.display = "none";
