6058c3646ed46eac44b5649e5e8dc33568e89cc4
Kropotov-Vadim <41684180+Kropotov-Vadim@users.noreply.github.com>
2018-12-17 12:51:11 +0300

    bino short

--- bino-short/js/main.js
+++ bino-short/js/main.js
@@ -1,5 +1,15 @@
 var linkNav = document.querySelectorAll('[href^="#"]'),
-  speed = 0.3;
+  speed = 0.3,
+  nameInput = document.getElementsByClassName("form_input")[0],
+  email = document.getElementsByClassName("form_input")[1],
+  subject = document.getElementsByClassName("form_input")[2],
+  textAreaForm = document.getElementsByClassName("form_textarea")[0],
+  subm = document.getElementsByClassName("btn-form")[0],
+  formThx= document.getElementsByClassName("form-thx")[0],
+  error = "error",
+  alright = "good",
+  menuMobile = document.getElementById("menu__mobile"),
+  menu = document.getElementsByClassName("menu")[0];
 
 for (var i = 0; i < linkNav.length; i++) {
   linkNav[i].addEventListener("click", function (e) {
@@ -24,9 +34,6 @@ for (var i = 0; i < linkNav.length; i++) {
   }, false);
 }
 
-var menuMobile = document.getElementById("menu__mobile"),
-  menu = document.getElementsByClassName("menu")[0];
-
 menuMobile.addEventListener("click", function () {
   menuMobile.classList.toggle("menu__mobile-active");
   menu.classList.toggle("menu-active");
@@ -41,96 +48,96 @@ $('.slider-wrapper').slick({
   nextArrow: '<div class="header__slide-next"></div> '
 });
 
-var namei = document.getElementsByClassName("form_input")[0],
-  mail = document.getElementsByClassName("form_input")[1],
-  subj = document.getElementsByClassName("form_input")[2],
-  tarea = document.getElementsByClassName("form_textarea")[0],
-  subm = document.getElementsByClassName("btn-form")[0],
-  formThx= document.getElementsByClassName("form-thx")[0],
-  error = "error",
-  alright = "good";
+$('.case__slider').slick({
+  dots: true,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
 
-mail.onblur = function () {
+email.onblur = function () {
   var result = this.value.match(/^[0-9a-z-\.]+\@[0-9a-z-]{1,}\.[a-z]{2,}$/i);
   if (!result) {
-    mail.classList.remove(alright);
-    mail.classList.add(error);
+    email.classList.remove(alright);
+    email.classList.add(error);
   } else {
-    mail.classList.remove(error);
-    mail.classList.add(alright);
+    email.classList.remove(error);
+    email.classList.add(alright);
   }
 }
 
-mail.onfocus = function () {
-  mail.classList.remove(error);
-  mail.classList.remove(alright);
+email.onfocus = function () {
+  email.classList.remove(error);
+  email.classList.remove(alright);
 }
 
-namei.onblur = function () {
-  if (namei.value == "") {
-    namei.classList.remove(alright);
-    namei.classList.add(error);
+nameInput.onblur = function () {
+  if (nameInput.value == "") {
+    nameInput.classList.remove(alright);
+    nameInput.classList.add(error);
   } else {
-    namei.classList.remove(error);
-    namei.classList.add(alright);
+    nameInput.classList.remove(error);
+    nameInput.classList.add(alright);
   }
 }
 
-namei.onfocus = function () {
-  namei.classList.remove(error);
-  namei.classList.remove(alright);
+nameInput.onfocus = function () {
+  nameInput.classList.remove(error);
+  nameInput.classList.remove(alright);
 }
 
-subj.onblur = function () {
-  if (subj.value == "") {
-    subj.classList.remove(alright);
-    subj.classList.add(error);
+subject.onblur = function () {
+  if (subject.value == "") {
+    subject.classList.remove(alright);
+    subject.classList.add(error);
   } else {
-    subj.classList.remove(error);
-    subj.classList.add(alright);
+    subject.classList.remove(error);
+    subject.classList.add(alright);
   }
 }
 
-subj.onfocus = function () {
-  subj.classList.remove("error");
-  subj.classList.remove(alright);
+subject.onfocus = function () {
+  subject.classList.remove(error);
+  subject.classList.remove(alright);
 }
 
-tarea.onblur = function () {
-  if (tarea.value == "") {
-    tarea.classList.remove(alright);
-    tarea.classList.add(error);
+textAreaForm.onblur = function () {
+  if (textAreaForm.value == "") {
+    textAreaForm.classList.remove(alright);
+    textAreaForm.classList.add(error);
   } else {
-    tarea.classList.remove(error);
-    tarea.classList.add(alright);
+    textAreaForm.classList.remove(error);
+    textAreaForm.classList.add(alright);
   }
 }
 
-tarea.onfocus = function () {
-  tarea.classList.remove(error);
-  tarea.classList.remove(alright);
+textAreaForm.onfocus = function () {
+  textAreaForm.classList.remove(error);
+  textAreaForm.classList.remove(alright);
 }
 
 subm.addEventListener("click", function() {
-  var result = mail.value.match(/^[0-9a-z-\.]+\@[0-9a-z-]{1,}\.[a-z]{2,}$/i);
+  var result = email.value.match(/^[0-9a-z-\.]+\@[0-9a-z-]{1,}\.[a-z]{2,}$/i);
 
-  if ( !result || namei.value == "" || tarea.value == "" || subj.value == "") {
+  if ( !result || nameInput.value == "" || textAreaForm.value == "" || subject.value == "") {
     event.preventDefault();
-    mail.classList.remove(alright);
-    tarea.classList.remove(alright);
-    subj.classList.remove(alright);
-    namei.classList.remove(alright);
-    mail.classList.add(error);
-    tarea.classList.add(error);
-    subj.classList.add(error);
-    namei.classList.add(error);
+    email.classList.remove(alright);
+    textAreaForm.classList.remove(alright);
+    subject.classList.remove(alright);
+    nameInput.classList.remove(alright);
+    email.classList.add(error);
+    textAreaForm.classList.add(error);
+    subject.classList.add(error);
+    nameInput.classList.add(error);
   } else {
     event.preventDefault();
     subm.style.display = "none";
-    mail.value = "";
-    tarea.value = "";
-    subj.value = "";
-    namei.value = "";
+    email.value = "";
+    textAreaForm.value = "";
+    subject.value = "";
+    nameInput.value = "";
     formThx.style.display = "block";
   }
 });
